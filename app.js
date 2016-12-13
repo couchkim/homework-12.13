09b66bb28bcbd161a559dd65027c1a6f875a8156
Kim Hanson <couchkim3800@gmail.com>
2016-12-13 14:52:29 -0500

    everything but formatting in js

--- app.js
+++ app.js
@@ -1,20 +1,51 @@
 function init(){
-    
+   
+let button1 = document.querySelector('#new');
+    button1.addEventListener('click', addProfile);
+
     let button2 = document.querySelector('#like');
     button2.addEventListener('click', function(){
-        console.log("Likes " + "username")
+        console.log("Likes " + "Thumbs Up Guy");
     })
 
     let button3 = document.querySelector('#nope');
         button3.addEventListener('click', function(){
-        console.log("username " + "has been noped!")
+        console.log("Thumbs Up Guy" + " has been noped!");
         })
+
 
 }
 
-let button1 = document.querySelector('#new');
-    button1.addEventListener('click', addProfile);
+function addProfile(){
+   
+   
+    let parent= document.createElement('li');
+   
+    let pic = document.createElement('img');
+    pic.src = "https://randomuser.me/api/portraits/men/3.jpg";
+    let name = document.createElement('h2');
+    name.textContent = "Thumbs Up Guy";
+    let like = document.createElement('button');
+    like.textContent = "Like";
+    like.addEventListener('click', function(){
+        console.log("Likes " + "Thumbs Up Guy");
+    })
+    let nope = document.createElement('button');
+    nope.textContent = "Nope";
+    nope.addEventListener('click', function(){
+        console.log("Thumbs Up Guy" + " has been noped!");
+    })
+ 
+    parent.appendChild(pic);
+    parent.appendChild(name);
+    parent.appendChild(like);
+    parent.appendChild(nope);
+    
 
+    let grandparent = document.querySelector('#list');
+    grandparent.appendChild(parent);
+    
+}
 
 
 window.addEventListener('load', init);
